990c30a601ddb4a9a7e31497bf593fbec839470b
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-09-25 19:43:03 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double horizontalMargin = 24.0;
+double verticalMargin = 30.0;
+double defaultRadius = 10.0;
+
+const Color kOrangeColor = Color(0xffF7AF4B);
+const Color kBlackColor = Color(0xff151619);
+const Color kGreyColor = Color(0xff6B6978);
+const Color kNavyColor = Color(0xff322E4A);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kOrangeColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
